48201a1e0fcd0f85b8c9c02baf31cfd765355bd7
zhujinxi <2775978161@qq.com>
2022-05-24 15:05:10 +0800

    Initial commit
    Signed-off-by: zhujinxi <2775978161@qq.com>

---
+++ Tets/src/test/tets.java
@@ -0,0 +1,7 @@
+package test;
+
+public class tets {
+    public static void main(String[] args) {
+        类.测试();
+    }
+}
---
+++ Tets/src/test/类.java
@@ -0,0 +1,7 @@
+package test;
+
+public class 类 {
+    public static void 测试() {
+        System.out.println("类.测试");
+    }
+}
